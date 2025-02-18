84f8a8348c05fddad552f6d2eeadf7d730ee003f
vigourpt <vigourpt@googlemail.com>
2025-02-18 23:31:06 +0000

    fix: improve module resolution for hooks

--- src/components/UserStatus/UserStatus.tsx
+++ src/components/UserStatus/UserStatus.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useUser } from '../../hooks/useUser';
+import { useUser } from '../../hooks';
 
 export const UserStatus: React.FC = () => {
   const { user } = useUser();
