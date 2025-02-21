b1f388137473f9d162657bfde44b4203f02ad144
vigourpt <vigourpt@googlemail.com>
2025-02-21 21:03:58 +0000

    fix: update Stripe API version to current stable

--- netlify/functions/create-checkout-session.ts
+++ netlify/functions/create-checkout-session.ts
@@ -2,7 +2,7 @@ import { Handler } from '@netlify/functions';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16'
+  apiVersion: '2025-01-27.acacia'
 });
 
 const PRICE_IDS = {
